8c9b245a1a2d02c46a3988135f0c42d0a46a88bf
Dilan Morar <DMorar@spartaglobal.com>
2019-12-04 14:22:52 +0000

    wrote two tests for tdd

---
+++ naan_factory.py
@@ -0,0 +1,10 @@
+# basis of a test
+    # testing functions or methods, and call ir initialise them
+    # having controlled inputs for known outputs
+    # testing for these
+
+# To make dough: mix water and flour to make dough
+print(make_dough('water','flour') == 'dough')
+# Baking dough: we should be able to put it in the oven and make naan
+
+print(bake_dough('dough') == 'Naan')
