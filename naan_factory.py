5eb9d4c8d28113c1e2cfe5e99098f7277b60fcdc
Dilan Morar <DMorar@spartaglobal.com>
2019-12-04 15:03:14 +0000

    tests are now passing

--- naan_factory.py
+++ naan_factory.py
@@ -4,17 +4,33 @@
     # testing for these
 
 def make_dough(arg1,arg2):
-    pass
+    if 'water' in [arg1, arg2] and 'flour' in [arg1,arg2]:
+        return 'dough'
+    else:
+        return 'not dough'
 
 def bake_dough(arg3):
-    pass
+    if arg3 == 'dough':
+        return 'naan'
+    else:
+        return 'not naan'
 
 # To make dough: mix water and flour to make dough
 print('testing make_dough expect "dough"')
 print(make_dough('water','flour') == 'dough')
 print('got: ', make_dough('water','flour'))
 
+# when i pass in anything other than flour and water i should get 'not dough'
+print('testing make_dough expect "not dough"')
+print(make_dough('ice','leaf') == 'not dough')
+print('got: ', make_dough('ice','leaf'))
+
 # Baking dough: we should be able to put it in the oven and make naan
 print('testing bake_dough expect "naan"')
 print(bake_dough('dough') == 'naan')
 print('got: ', bake_dough('dough'))
+
+# when i pass in anything other than dough i should get 'not naan'
+print('testing bake_dough expect "not naan"')
+print(bake_dough('chicken') == 'not naan')
+print('got: ', bake_dough('chicken'))
